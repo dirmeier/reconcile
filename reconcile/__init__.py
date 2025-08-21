ab4cd94092af594f709033fa87b5a7ee98212f58
Simon Dirmeier <simd23@protonmail.com>
2025-08-21 15:01:41 +0200

    Update for later JAX versions

--- reconcile/__init__.py
+++ reconcile/__init__.py
@@ -1,6 +1,6 @@
 """reconcile: Probabilistic reconciliation of time series forecasts."""
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 from reconcile.forecast import Forecaster
 from reconcile.grouping import Grouping
